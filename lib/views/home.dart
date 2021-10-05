a7a91514fe165dfef0ee8ef2799b8f145d22b0dc
Simran1604 <simran251601@gmail.com>
2021-10-05 16:39:23 +0530

    Modifies ArticleTile widget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -180,38 +180,80 @@ class CategoryTile extends StatelessWidget {
 }
 
 class ArticleTile extends StatelessWidget {
-  
+
   final String imageUrl, title, desc,url;
   ArticleTile({@required this.imageUrl, @required this.title, @required this.desc, @required this.url});
-  
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: (){
         Navigator.push(context, MaterialPageRoute(
-            builder: (context) => ArticleView(
-              articleUrl: url,
-            ),
+          builder: (context) => ArticleView(
+            articleUrl: url,
+          ),
         ));
 
       },
       child: Container(
+        color: Colors.black,
         padding: EdgeInsets.fromLTRB(16, 0, 16, 10),
-        child: Column(
-          children: <Widget>[
-            ClipRRect(borderRadius: BorderRadius.circular(6),
-                child: Image.network(imageUrl,fit: BoxFit.cover)),
-            Text(title,style: TextStyle(
-              fontSize: 17,
-              color: Colors.white,
-            ),),
-            Text(desc,style: TextStyle(
-              fontSize: 14, color: Colors.grey,
-            ),),
-          ],
+        child: Card(
+          color: Colors.grey.shade900,
+          elevation: 10,
+          shadowColor: Colors.grey.shade300,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:18.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height/4,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: Image.network(imageUrl,fit: BoxFit.cover)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:8.0),
+                child: SizedBox(                  
+                width: MediaQuery.of(context).size.width/1.2,
+                  child: Text(title,style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.white,
+                ),),),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:8.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height/4,
+                  width: MediaQuery.of(context).size.width/1.2,
+                  child: Text(desc,style: TextStyle(
+                    fontSize: 16, color: Colors.grey,
+                  ),),
+                ),
+              ),
+              // DecoratedBox(decoration: 
+              // BoxDecoration(),
+              // child: Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //     children: [
+              //       Icon(
+              //         Icons.share,color: Colors.deepOrange,size:30
+              //       ),
+              //       Icon(
+              //         Icons.bookmark,color: Colors.deepOrange,size: 30,
+              //         )
+              //     ],
+              //   ),
+              // ),)
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
 
